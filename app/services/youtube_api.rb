ca03cdb63a106c45ee4967040cffa18a1224f19d
Elyse <elyse-groenendaal@outlook.com>
2021-08-14 16:26:57 +0900

    credentials change

--- app/services/youtube_api.rb
+++ app/services/youtube_api.rb
@@ -10,7 +10,7 @@ class YoutubeApi
     require 'json'
 
     # REPLACE WITH VALID @redirect_uri FOR YOUR CLIENT
-    @redirect_uri = 'http://localhost:3000/oauth2callback'
+    @redirect_uri = 'https://freescriber.herokuapp.com/oauth2callback'
     @application_name = 'freescriber'
 
     # REPLACE WITH NAME/LOCATION OF YOUR client_secrets.json FILE
@@ -91,7 +91,7 @@ class YoutubeApi
         puts "Open the following URL in the browser and enter the " +
             "resulting code after authorization"
         puts url
-        code = gets
+        code = ENV['YOUTUBE_TOKEN']
         @credentials = authorizer.get_and_store_credentials_from_code(
           user_id: user_id, code: code, base_url: @redirect_uri)
       end
